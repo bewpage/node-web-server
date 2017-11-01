6cb283aa5fde087d760fee8fb22bcbdfd813761f
bewpage <b.jedlinski@bewpage.com>
2017-11-01 10:03:30 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+   let now = new Date().toString();
+   let log = `${now}: ${req.method} ${req.url}`;
+   console.log(log);
+   //'\n' this is character to make new line in log file
+   fs.appendFile('server.log', log + '\n', (err) => {
+       if(err){
+           console.log('Unable to append to server.log')
+       }
+   });
+   next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+//we not call next and we stop here
+});
+//to not show other subpages we have to move it here
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('toUpperCase', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        message: 'welcome message'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        message: 'About message'
+    });
+});
+
+app.get('/bad', (req, res) => {
+   res.send({
+       errorMessage: 'sorry, somthing went wrong'
+   })
+});
+
+app.listen(3000);
